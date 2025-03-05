74f8d5ecc1f9f497ab56c7500ff5aa99c2142fee
soeiroo <friskylt8@gmail.com>
2025-03-05 15:03:13 -0300

    Organizando o código

--- src/edu/israel/primeirasemana/miniprojeto/Animal.java
+++ src/edu/israel/primeirasemana/miniprojeto/Animal.java
@@ -11,7 +11,7 @@ public class Animal {
   private int id;
 
 
-  //Construtor
+  //Constructor
   public Animal(String name, String species, int age, int id){
     this.name = name;
     this.species = species;
@@ -51,34 +51,56 @@ public class Animal {
     this.id = id;
   }
 
-  public static void main(String[] args) {
-    final String FILE_PATH = "animals-info.json";
-    List<Animal> animals = new ArrayList<Animal>();
-    System.out.println(animals);
 
-    Scanner reader = new Scanner(System.in);
-
-    while (true){
-      System.out.print("[1] Cadastrar um animal\n[2] Listar animais\n[3] Sair\n");
-      int option = reader.nextInt();
-
-      switch (option) {
-        case 1 -> cadastrarAnimal();
-
-        //case 2 -> listarAnimais();}
-
-    }
-  }
-
-
-  private static void cadastrarAnimal() {
+class AnimalRegistry{
+  private void registerAnimal() {
     Scanner reader = new Scanner(System.in);
     System.out.print("Digite o nome do animal: ");
     String name = reader.nextLine();
     System.out.print("Digite a espécie do animal: ");
     String species = reader.nextLine();
     System.out.print("Digite a idade do animal: ");
-    int age = reader.nextInt();
+    int age;
+    do {
+      System.out.println("Digite a idade do animal (número positivo): ");
+      while (!reader.hasNextInt()) {
+        System.out.print("Digite um número válido para a idade: ");
+        reader.next();
+      }
+      age = reader.nextInt();
+    } while (age <= 0);
+
+
+
+    System.out.println("Animal cadastrado com sucesso!");
+
   }
 
+}
+
+
+
+public static void main(String[] args) {
+  final String FILE_PATH = "animals-info.json";
+  List<Animal> animals = new ArrayList<Animal>();
+  //Ainda a ser implementado
+
+
+
+
+  Scanner reader = new Scanner(System.in);
+
+  while (true){
+    System.out.print("[1] Cadastrar um animal\n[2] Listar animais\n[3] Sair\n");
+    int option = reader.nextInt();
+
+    switch (option) {
+      case 1: {
+
+      }
+      //case 1 -> registerAnimal();
+      //case 2 -> listAnimals();}
+    }
+  }
+}
 }
